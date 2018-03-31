349f8998abd664fc1f61fe49ae9f80dd0eded930
Gavin Abson <gavin.abson@gmail.com>
2018-03-31 21:03:54 +0100

    Stop making symlinks for lost+found

--- do_backups.sh
+++ do_backups.sh
@@ -17,6 +17,9 @@ rm -f $BACKUP_DIR/2*-*
 
 # Create symlinks with date and time in the backups dir
 while read -r file
-do 
-  ln -s $BACKUP_DIR/$file $BACKUP_DIR/`stat -c %y $BACKUP_DIR/$file | sed 's/\..*//g' | sed 's/://g' | sed 's/-/./g' | sed 's/ /-/g'`
+do
+  if [ "$file" != "lost+found" ]
+  then
+    ln -s $BACKUP_DIR/$file $BACKUP_DIR/`stat -c %y $BACKUP_DIR/$file | sed 's/\..*//g' | sed 's/://g' | sed 's/-/./g' | sed 's/ /-/g'`
+  fi
 done<<<"`ls -1 $BACKUP_DIR`"
