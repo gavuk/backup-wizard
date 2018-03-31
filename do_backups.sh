9534fa3ba7019545dd74e110e3e9272b436f30fd
Gavin Abson <gavin.abson@gmail.com>
2018-03-31 20:36:01 +0100

    Create symlinks when backing up

--- do_backups.sh
+++ do_backups.sh
@@ -11,3 +11,12 @@ while read -r conf
 do
    rsnapshot -c $conf $arg
 done<<<"`find $EXEC_DIR/etc/ -iname '*.conf' -type f`"
+
+# Remove existing symlinks
+rm -f $BACKUP_DIR/2*-*
+
+# Create symlinks with date and time in the backups dir
+while read -r file
+do 
+  ln -s $BACKUP_DIR/$file `stat -c %y $BACKUP_DIR/$file | sed 's/\..*//g' | sed 's/://g' | sed 's/-/./g' | sed 's/ /-/g'`
+done<<<"`ls -1 $BACKUP_DIR`"
