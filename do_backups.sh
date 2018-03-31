79c6946df487614acfa791ad784681225a970c81
Gavin Abson <gavin.abson@gmail.com>
2018-04-01 00:30:06 +0100

    Changed rsnapshot to start in background

--- do_backups.sh
+++ do_backups.sh
@@ -9,7 +9,7 @@ arg="$1"
 # List all of the rsnapshot configs
 while read -r conf
 do
-   rsnapshot -c $conf $arg
+   rsnapshot -c $conf $arg &
 done<<<"`find $EXEC_DIR/etc/ -iname '*.conf' -type f`"
 
 # Remove existing symlinks
