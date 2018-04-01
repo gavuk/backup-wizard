c589f871813e97b5d0b41b439164bd9b40012b13
Gavin Abson <gavin.abson@gmail.com>
2018-04-01 01:24:09 +0100

    Split symlinks into separate script

--- do_backups.sh
+++ do_backups.sh
@@ -14,12 +14,3 @@ done<<<"`find $EXEC_DIR/etc/ -iname '*.conf' -type f`"
 
 # Remove existing symlinks
 rm -f $BACKUP_DIR/2*-*
-
-# Create symlinks with date and time in the backups dir
-while read -r file
-do
-  if [ "$file" != "lost+found" ]
-  then
-    ln -s $BACKUP_DIR/$file $BACKUP_DIR/`stat -c %y $BACKUP_DIR/$file | sed 's/\..*//g' | sed 's/://g' | sed 's/-/./g' | sed 's/ /-/g'`
-  fi
-done<<<"`ls -1 $BACKUP_DIR`"
