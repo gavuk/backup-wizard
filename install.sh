37835367b783feadbc522235e18eb3bb299c17cf
Gavin Abson <gavin.abson@gmail.com>
2018-04-02 17:16:04 +0100

    Changed cron job times

--- install.sh
+++ install.sh
@@ -8,9 +8,9 @@ apt-get -y install rsnapshot
 ssh-keygen -t rsa -N "" -f ~/.ssh/id_rsa
 
 # Set up the cron jobs
-echo "0 4 * * * `pwd`/do_backups.sh daily" >> /var/spool/cron/crontabs/root
-echo "5 4 * * * `pwd`/do_symlinks.sh" >> /var/spool/cron/crontabs/root
-echo "0 2 * * 7 `pwd`/do_backups.sh weekly" >> /var/spool/cron/crontabs/root
-echo "5 2 * * 7 `pwd`/do_symlinks.sh" >> /var/spool/cron/crontabs/root
-echo "0 4 1 * * `pwd`/do_backups.sh monthly" >> /var/spool/cron/crontabs/root
-echo "5 4 1 * * `pwd`/do_symlinks.sh" >> /var/spool/cron/crontabs/root
+echo "0 6 * * * `pwd`/do_backups.sh daily" >> /var/spool/cron/crontabs/root
+echo "5 6 * * * `pwd`/do_symlinks.sh" >> /var/spool/cron/crontabs/root
+echo "0 4 * * 7 `pwd`/do_backups.sh weekly" >> /var/spool/cron/crontabs/root
+echo "5 4 * * 7 `pwd`/do_symlinks.sh" >> /var/spool/cron/crontabs/root
+echo "0 2 1 * * `pwd`/do_backups.sh monthly" >> /var/spool/cron/crontabs/root
+echo "5 2 1 * * `pwd`/do_symlinks.sh" >> /var/spool/cron/crontabs/root
