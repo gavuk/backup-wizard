7ea9962325379165aa4b75e9c0daf7f8d37f6b7d
Gavin Abson <gavin.abson@gmail.com>
2018-04-02 16:51:22 +0100

    Added symlinks to crontab when installing

--- install.sh
+++ install.sh
@@ -9,5 +9,8 @@ ssh-keygen -t rsa -N "" -f ~/.ssh/id_rsa
 
 # Set up the cron jobs
 echo "0 4 * * * `pwd`/do_backups.sh daily" >> /var/spool/cron/crontabs/root
+echo "5 4 * * * `pwd`/do_symlinks.sh" >> /var/spool/cron/crontabs/root
 echo "0 2 * * 7 `pwd`/do_backups.sh weekly" >> /var/spool/cron/crontabs/root
+echo "5 2 * * 7 `pwd`/do_symlinks.sh" >> /var/spool/cron/crontabs/root
 echo "0 4 1 * * `pwd`/do_backups.sh monthly" >> /var/spool/cron/crontabs/root
+echo "5 4 1 * * `pwd`/do_symlinks.sh" >> /var/spool/cron/crontabs/root
