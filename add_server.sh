33bad4d61a9ea9db8ef377c8434c81ebb3cf4242
Gavin Abson <gavin.abson@gmail.com>
2018-04-02 15:55:06 +0100

    Changed DB backup to dump/* to dump/*.sql

--- add_server.sh
+++ add_server.sh
@@ -151,7 +151,7 @@ function do_rsnapshot_mysql {
 
   # Write the line to the rsnapshot conf file
   echo -e "backup_exec\tssh $user@$hostname 'rm -f /var/db/dump/*; while read -r db; do mysqldump --skip-lock-tables \$db > /var/db/dump/\$db.sql; done<<<\"\`mysql -Ne \"show databases\" | grep -vxe \"information_schema\|sys\"\`\"'" >> etc/$hostname.conf
-  echo -e "backup\t$user@$hostname:/var/db/dump/\t$hostname/" >> etc/$hostname.conf
+  echo -e "backup\t$user@$hostname:/var/db/dump/*.sql\t$hostname/" >> etc/$hostname.conf
 
 }
 
